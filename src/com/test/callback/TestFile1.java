70ef31752f05789ae50b7586575309d51f94ddcd
michael chen <michaelchen@michealdeMacBook-Pro.local>
2020-12-22 20:14:16 +0800

    提交feature-03

---
+++ src/com/test/callback/TestFile1.java
@@ -0,0 +1,14 @@
+package com.test.callback;
+
+/**
+ * @ClassName TestFile1
+ * @Description 343
+ * @Author chenle
+ * @Date 2020-12-22 20:13
+ */
+public class TestFile1 {
+
+    public static void main(String[] args){
+        System.out.println(">>>>>>>测试123>>>>>>>>");
+    }
+}
